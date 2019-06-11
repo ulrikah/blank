d268c074fe0f1db6fd7a662f9b916c4b81c2c51a
ulrikah <ulrikhalmoy@gmail.com>
2019-06-11 16:00:54 +0200

    A-Frame interaction with Tone

---
+++ components/play-sample.js
@@ -0,0 +1,28 @@
+const FOLDER = "../assets/samples/"
+const SAMPLES = ['a', 'b', 'c'];
+const SUFFIX = ".mp3"
+
+
+// Component to change to play an audio sample from Tone.js
+AFRAME.registerComponent('play-sample', {
+	schema: {
+    type: 'string',
+    default: 'C1'
+  },
+
+  init: function () {
+  	const notes = ["C1", "D1", "E1"];
+    const sampler = new Tone.Sampler({
+			"C1" : "../assets/samples/a.mp3",
+			"D1" : "../assets/samples/b.mp3",
+			"E1" : "../assets/samples/c.mp3"
+		}, function(){
+			// sampler will trigger on load
+			sampler.triggerAttackRelease("C1")
+		}).toMaster()
+
+    this.el.addEventListener('click', function (evt) {
+      sampler.triggerAttackRelease(notes[Math.floor(Math.random()*notes.length)])
+    });
+  }
+});
--- main.js
+++ main.js
@@ -1,2 +1,8 @@
+// npm imports
 const aframe = require('aframe')
-const tone = require('tone') 
+// const tone = require('tone') // TO DO: get Tone to work as a module
+
+
+// AFRAME components
+const cursor = require('./components/cursor-listener.js')
+const sampler = require('./components/play-sample.js')
