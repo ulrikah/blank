9fd058e2cbb7526179f97eda18d63160b1a2dddf
ulrikah <ulrikhalmoy@gmail.com>
2019-07-10 14:19:40 +0200

    update to make sound source relative to camera, not only the object

--- components/panner.js
+++ components/panner.js
@@ -1,21 +1,28 @@
 const Tone = require('tone');
 
-const panner = new Tone.Panner3D(10, 0, 0).toMaster();
-const player = new Tone.Player("../assets/100bpm_luftig.mp3")
-player.connect(panner).sync()
-player.autostart = true
-
 AFRAME.registerComponent('panner', {
 	
   init: function () {
-  	player.start(0)
+		this.panner = new Tone.Panner3D(0, 0, 0).toMaster()
+		this.player = new Tone.Player("../assets/100bpm_luftig.mp3")
+		this.player.autostart = true;
+		this.player.connect(this.panner).sync().start(0)
+  	
+  	this.camera = document.querySelector('a-camera')
+  	this.i = 0;
+  	console.log(this.el);
+
   	this.pos = new THREE.Vector3();
   },
 
   tick: function () {
   	this.updateDelta()
-  	panner.setPosition(-this.pos.x, this.pos.y, this.pos.z) // Tone and THREE operates with inverse x-pos
-
+  	diff = { 
+  		x: this.camera.object3D.position.x - this.pos.x,
+  		y: this.camera.object3D.position.y - this.pos.y,
+  		z: this.camera.object3D.position.z - this.pos.z,
+  	}
+  	this.panner.setPosition(diff.x, diff.y, diff.z)
   },
 
   updateDelta: function () {
