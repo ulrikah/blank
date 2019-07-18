9e24afc583cac2558036be5921e2396af1b465ba
ulrikah <ulrikhalmoy@gmail.com>
2019-07-18 16:24:23 +0200

    randomize velocity of bouncing objects

--- components/shooter.js
+++ components/shooter.js
@@ -1,3 +1,5 @@
+const fn = require('../functions.js');
+
 AFRAME.registerComponent('shooter', {
 	
   init: function () {
@@ -16,19 +18,25 @@ AFRAME.registerComponent('shooter', {
 
   shoot: function () {
     const ammo = this.createAmmo()
-    ammo.setAttribute('velocity', '0 2 -10');
+    ammo.setAttribute('velocity', [fn.randomInt(-10, 10), fn.randomInt(-10, 10), fn.randomInt(0, 10)].join(" "));
     AFRAME.scenes[0].appendChild(ammo);
     this.el.components.sound.playSound();
   },
 
   createAmmo: function () {
   	const ammo = document.createElement('a-entity');
-    ammo.setAttribute('dynamic-body', { shape: 'box', linearDamping: 0.1, angularDamping: 0.1});
+    ammo.setAttribute('dynamic-body', { shape: 'box'});
     ammo.setAttribute('geometry', { primitive: 'box', width: 0.2, depth: 0.2, height: 0.2})
     ammo.setAttribute('position', this.position);
     ammo.setAttribute('material', { roughness: 0.48, color: "#edaa45"});
     ammo.setAttribute('shadow', { cast: true, receive: true })
   	return ammo;
-  }
+  },
+
+  randomInt: function (min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
 
 });
