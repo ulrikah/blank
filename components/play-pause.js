130580bd427f89f5fac1a3cc401364df583abb65
ulrikah <ulrikhalmoy@gmail.com>
2019-07-26 10:41:04 +0200

    apply changes to Tone.Master.volume when toggling play/pause to truly pause all instruments

--- components/play-pause.js
+++ components/play-pause.js
@@ -5,17 +5,25 @@ AFRAME.registerComponent('play-pause', {
   init: function () {
   	const playCol = "#7087CC";
   	const pauseCol = "#F6F7A7";
+  	const el = this.el;
+  	Tone.Master.volume.value = 0;
   	['click', 'grabend'].forEach(e => this.el.addEventListener(e, function (evt) {
-  		const color = evt.target.getAttribute('material').color;
-  		Tone.Transport.toggle("+0.1")
+  		const color = el.getAttribute('material').color;
+  		if (Tone.Transport.state === "stopped" || Tone.Transport.state === "paused")
+  		{
+  			Tone.Transport.start("+0.1")
+  			Tone.Master.volume.exponentialRampToValueAtTime('1', 5)
+				el.setAttribute('material', 'color', playCol)
+				el.setAttribute('rotate', '')
+  		}
 
-			if (color === pauseCol) {
-				evt.target.setAttribute('material', 'color', playCol)
-				evt.target.setAttribute('rotate', '')
-			} else {
-				evt.target.setAttribute('material', 'color', pauseCol)
-				evt.target.removeAttribute('rotate')
-			}
+  		else if (Tone.Transport.state === "started")
+  		{
+  			Tone.Master.volume.linearRampToValueAtTime('-100', 10)
+  			Tone.Transport.stop("+0.1")
+				el.setAttribute('material', 'color', pauseCol)
+				el.removeAttribute('rotate')
+  		}
     }));
   }
 });
